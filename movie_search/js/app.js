36eb2c9fc91da4ca27d596dd46cb42464ad1be73
Scott Christensen <scottmc526@gmail.com>
2016-02-24 11:36:53 -0700

    movie id in url

--- movie_search/js/app.js
+++ movie_search/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('movie_search', ['ngRoute'])
 
 app.config(function($routeProvider) {
   $routeProvider
-    .when('/show', {
+    .when('/:imdbID/show', {
     templateUrl: 'partials/show.html',
     controller: 'movieController'
   })
