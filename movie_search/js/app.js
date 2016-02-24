1e98da30206def0c85ce3e8c6da85ee9b2c7835b
Scott Christensen <scottmc526@gmail.com>
2016-02-24 11:33:59 -0700

    show page loads

--- movie_search/js/app.js
+++ movie_search/js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('movie_search', ['ngRoute'])
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/show', {
+    templateUrl: 'partials/show.html',
+    controller: 'movieController'
+  })
+})
---
+++ movie_search/js/controllers/movie_controller.js
@@ -0,0 +1,3 @@
+app.controller('movieController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  $scope.hello = 'hello'
+}])
