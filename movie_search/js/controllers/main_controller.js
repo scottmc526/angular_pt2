43719239ffe06550e7723cbee1528e5340b7ac68
Scott Christensen <scottmc526@gmail.com>
2016-02-23 17:51:25 -0700

    movie tites and pics show up in div

--- movie_search/js/controllers/main_controller.js
+++ movie_search/js/controllers/main_controller.js
@@ -1,14 +1,9 @@
 app.controller('mainController', ['$scope', '$http', function($scope, $http){
 
   $scope.search = function() {
-    $scope.titles = [];
+    $scope.movies = [];
     $http.get('http://omdbapi.com/?s='+$scope.input).then(function(data){
-      $scope.movies = data.data.Search;
-        for (var i =0; i< $scope.movies.length; i++){
-          var title = $scope.movies[i]['Title'];
-          $scope.titles.push(title)
-        }
-        console.log($scope.titles);
+      console.log($scope.movies = data.data.Search);
     });
 
   }
