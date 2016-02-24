bc30b8800954c4770c1aac6dad31c56e3d5dfaa2
Scott Christensen <scottmc526@gmail.com>
2016-02-23 17:04:35 -0700

    successfully getting one movie back from api

--- movie_search/js/controllers/main_controller.js
+++ movie_search/js/controllers/main_controller.js
@@ -1,3 +1,9 @@
-app.controller('mainController', ['$scope', function($scope){
-  $scope.where = 'Home';
+app.controller('mainController', ['$scope', '$http', function($scope, $http){
+
+  $scope.search = function() {
+    $http.get('http://omdbapi.com/?t='+$scope.input+'&r=json').then(function(data){
+      $scope.movies = data.data;
+      console.log($scope.movies);
+    });
+  }
 }])
