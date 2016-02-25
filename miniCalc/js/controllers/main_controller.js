559c02068a63ad25cf9fa9a12866e75f94f5aa97
Scott Christensen <scottmc526@gmail.com>
2016-02-25 13:32:41 -0700

    calculator functions

--- miniCalc/js/controllers/main_controller.js
+++ miniCalc/js/controllers/main_controller.js
@@ -1,3 +1,26 @@
-app.controller('mainController', ['$scope', function($scope){
-  $scope.sayHi = 'hi'
+app.controller('mainController', ['$scope', 'calcService', function($scope, calcService){
+  $scope.math = function() {
+    $scope.value = document.getElementById('operation').value;
+
+    switch ($scope.value) {
+      case '1':
+          $scope.total = calcService.add(+$scope.num1, +$scope.num2)
+        break;
+      case '2':
+          $scope.total = calcService.subtract(+$scope.num1, +$scope.num2)
+        break;
+      case '3':
+          $scope.total = calcService.multiply(+$scope.num1, +$scope.num2)
+        break;
+      case '4':
+          $scope.total = calcService.divide(+$scope.num1, +$scope.num2)
+        break;
+      case '5':
+          $scope.total = calcService.power(+$scope.num1, +$scope.num2)
+      default: console.log('didnt work');
+
+    }
+  }
+
+
 }])
---
+++ miniCalc/js/services.js
@@ -0,0 +1,19 @@
+app.service('calcService', [function(){
+  return {
+    add: function(num1, num2){
+      return num1 + num2;
+    },
+    subtract: function(num1, num2){
+      return num1 - num2;
+    },
+    multiply: function(num1, num2){
+      return num1 * num2;
+    },
+    divide: function(num1, num2){
+      return num1 / num2;
+    },
+    power: function(num1, num2){
+      return Math.pow(num1,num2)
+    }
+  }
+}])
