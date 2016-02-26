c6cf7ef237614174b7b194b295f79a97bf2a606d
Scott Christensen <scottmc526@gmail.com>
2016-02-26 14:05:11 -0700

    ready to add info from json to table

--- shopping_cart/js/controllers/main_controller.js
+++ shopping_cart/js/controllers/main_controller.js
@@ -1,7 +1,7 @@
 app.controller('mainController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
   $scope.teas = function(){
     teaGetter.getTea().then(function(data){
-      console.log(data);
+      console.log(data.data[0]['ingredients']);
     })
 
   }
