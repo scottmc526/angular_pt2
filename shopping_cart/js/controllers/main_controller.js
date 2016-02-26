18838f5c9bf07a776af2430d2e9a39ec0738e099
Scott Christensen <scottmc526@gmail.com>
2016-02-26 15:53:11 -0700

    css on main page complete

--- shopping_cart/js/controllers/main_controller.js
+++ shopping_cart/js/controllers/main_controller.js
@@ -1,8 +1,10 @@
 app.controller('mainController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
-  $scope.teas = function(){
+  $scope.categories = [];
     teaGetter.getTea().then(function(data){
-      console.log(data.data[0]['ingredients']);
+      $scope.teas = data.data
+      // for (var i = 0; i < $scope.teas.length; i++) {
+      //   $scope.categories.push($scope.teas[i]['categories'])
+      // }
+      // console.log($scope.categories);
     })
-
-  }
 }])
