ec806afc1a97ab927024d143cac943813ac2c9cf
Scott Christensen <scottmc526@gmail.com>
2016-02-27 16:22:29 -0700

    subtotal shows correctly

--- shopping_cart/js/controllers/checkout_controller.js
+++ shopping_cart/js/controllers/checkout_controller.js
@@ -1,4 +1,4 @@
 app.controller('checkoutController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
   $scope.cart = teaGetter.cart
-  $scope.total = $scope.cart.price * $scope.cart.qty
+
 }])
--- shopping_cart/js/services.js
+++ shopping_cart/js/services.js
@@ -6,9 +6,7 @@ app.service('teaGetter', ['$http', function($http){
     },
     addCart: function(input,qty) {
       qty == undefined ? input.qty = 1 : input.qty = Number(qty)
-      // input.qty = qty
       this.cart.push(input)
-      console.log(this.cart);
     }
   }
 }])
