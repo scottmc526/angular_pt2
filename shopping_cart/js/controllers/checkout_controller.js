466f5d88a4036009e05dd8c6bdafbe72fb75cac1
Scott Christensen <scottmc526@gmail.com>
2016-02-27 16:39:45 -0700

    grand total correct

--- shopping_cart/js/controllers/checkout_controller.js
+++ shopping_cart/js/controllers/checkout_controller.js
@@ -1,4 +1,7 @@
 app.controller('checkoutController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
   $scope.cart = teaGetter.cart
-
+  $scope.grandTotal = 0;
+  for (var i = 0; i < $scope.cart.length; i++) {
+    $scope.grandTotal += $scope.cart[i]['price'] * $scope.cart[i]['qty']
+  }
 }])
--- shopping_cart/js/services.js
+++ shopping_cart/js/services.js
@@ -1,12 +1,19 @@
 app.service('teaGetter', ['$http', function($http){
   return {
     cart: [],
+    total: 0,
     getTea: function(){
       return $http.get('/tea_list.json')
     },
     addCart: function(input,qty) {
       qty == undefined ? input.qty = 1 : input.qty = Number(qty)
       this.cart.push(input)
+
+    },
+    totalz: function(){
+      for (var i = 0; i < this.cart.length; i++) {
+        return this.total += this.cart[i]['price'] * this.cart[i]['qty']
+      }
     }
   }
 }])
