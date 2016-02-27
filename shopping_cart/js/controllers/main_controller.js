f970c91ae806b4726f93f7d66247e29ce9307ce8
Scott Christensen <scottmc526@gmail.com>
2016-02-26 19:24:17 -0700

    add object to cart

--- shopping_cart/js/controllers/main_controller.js
+++ shopping_cart/js/controllers/main_controller.js
@@ -1,10 +1,10 @@
 app.controller('mainController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
-  $scope.categories = [];
-    teaGetter.getTea().then(function(data){
-      $scope.teas = data.data
-      // for (var i = 0; i < $scope.teas.length; i++) {
-      //   $scope.categories.push($scope.teas[i]['categories'])
-      // }
-      // console.log($scope.categories);
-    })
+  $scope.cart = teaGetter.cart;
+  teaGetter.getTea().then(function(data){
+    $scope.teas = data.data
+  })
+  $scope.add = function(input) {
+    teaGetter.addCart(input)
+    console.log($scope.cart);
+  }
 }])
--- shopping_cart/js/services.js
+++ shopping_cart/js/services.js
@@ -1,7 +1,11 @@
 app.service('teaGetter', ['$http', function($http){
   return {
+    cart: [],
     getTea: function(){
       return $http.get('/tea_list.json')
+    },
+    addCart: function(input) {
+      this.cart.push(input)
     }
   }
 }])
