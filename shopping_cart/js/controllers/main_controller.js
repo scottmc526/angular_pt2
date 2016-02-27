58d3092b4e8fd5eeac54a81f327000705999c590
Scott Christensen <scottmc526@gmail.com>
2016-02-26 19:42:39 -0700

    bag value updates

--- shopping_cart/js/controllers/main_controller.js
+++ shopping_cart/js/controllers/main_controller.js
@@ -1,10 +1,13 @@
 app.controller('mainController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
   $scope.cart = teaGetter.cart;
+  $scope.emptyBag = 'Empty!'
+  $scope.bagValue = 0;
+
   teaGetter.getTea().then(function(data){
     $scope.teas = data.data
   })
   $scope.add = function(input) {
     teaGetter.addCart(input)
-    console.log($scope.cart);
+    $scope.bagValue++
   }
 }])
