12e63ffa69e398965f06d1369a6f11ec0174293e
Scott Christensen <scottmc526@gmail.com>
2016-02-26 20:20:39 -0700

    refactored addCart function

--- shopping_cart/js/services.js
+++ shopping_cart/js/services.js
@@ -5,11 +5,7 @@ app.service('teaGetter', ['$http', function($http){
       return $http.get('/tea_list.json')
     },
     addCart: function(input,qty) {
-      if(document.getElementById('qty').value === ''){
-        input.qty = 1
-      }else {
-        input.qty = Number(document.getElementById('qty').value);
-      }
+      document.getElementById('qty').value === '' ? input.qty = 1 : input.qty = Number(document.getElementById('qty').value);
       this.cart.push(input)
     }
   }
