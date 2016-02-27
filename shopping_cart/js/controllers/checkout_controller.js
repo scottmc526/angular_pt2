beab34a3388e42d23f0238a69c629e386682d770
Scott Christensen <scottmc526@gmail.com>
2016-02-27 10:35:07 -0700

    checkout style

--- shopping_cart/js/controllers/checkout_controller.js
+++ shopping_cart/js/controllers/checkout_controller.js
@@ -1,3 +1,6 @@
 app.controller('checkoutController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
-  $scope.hello = 'hello'
+  $scope.cart = teaGetter.cart
+  $scope.total = $scope.cart.price * $scope.cart.qty
+  console.log($scope.cart.price);
+  console.log($scope.cart.qty);
 }])
