3ebf7e3a7c91bfa2b4ecb5b1c5e9a1c5fca88ef1
Scott Christensen <scottmc526@gmail.com>
2016-02-27 09:50:51 -0700

    content shows on /home

--- shopping_cart/js/app.js
+++ shopping_cart/js/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('cart', ['ngRoute'])
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'partials/index.html',
+      controller: 'mainController'
+    })
+    .when('/checkout', {
+    templateUrl: 'partials/checkout.html',
+    controller: 'checkoutController'
+  })
+})
