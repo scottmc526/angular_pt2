d7cda3bbb9a1b69dba6646df4cfd0022f7da0f6f
Scott Christensen <scottmc526@gmail.com>
2016-02-29 13:02:04 -0700

    price updates if qty is more than one

--- shopping_cart/js/controllers/checkout_controller.js
+++ shopping_cart/js/controllers/checkout_controller.js
@@ -2,18 +2,14 @@ app.controller('checkoutController', ['teaGetter', '$scope', '$http', function(t
   $scope.cart = teaGetter.cart
   $scope.grandTotal = 0;
   $scope.show = false;
+
   for (var i = 0; i < $scope.cart.length; i++) {
     $scope.grandTotal += $scope.cart[i]['price'] * $scope.cart[i]['qty']
   }
 
   $scope.delete = function(input, price){
-    price = $scope.cart[input]['price']
+    price = $scope.cart[input]['price'] * $scope.cart[input]['qty']
     teaGetter.deleteCart(input)
     $scope.grandTotal -= price
   }
-  // $scope.showEdit = function(){
-  //   $scope.show = true;
-  // }
-  // $scope.edit = function(qty){
-  // }
 }])
