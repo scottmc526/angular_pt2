118668b8fbc78899f42bfb645606636f7ec0518b
Scott Christensen <scottmc526@gmail.com>
2016-02-29 13:20:08 -0700

    edit select box

--- shopping_cart/js/controllers/checkout_controller.js
+++ shopping_cart/js/controllers/checkout_controller.js
@@ -1,7 +1,7 @@
 app.controller('checkoutController', ['teaGetter', '$scope', '$http', function(teaGetter, $scope, $http){
   $scope.cart = teaGetter.cart
   $scope.grandTotal = 0;
-  $scope.show = false;
+  $scope.editBox = false;
 
   for (var i = 0; i < $scope.cart.length; i++) {
     $scope.grandTotal += $scope.cart[i]['price'] * $scope.cart[i]['qty']
@@ -12,7 +12,10 @@ app.controller('checkoutController', ['teaGetter', '$scope', '$http', function(t
     teaGetter.deleteCart(input)
     $scope.grandTotal -= price
   }
-  $scoppe.edit =  function(){
+  $scope.edit =  function(){
     $scope.show = true;
+  }
+  $scope.toggle = function() {
+    $scope.editBox = !$scope.editBox
   }
 }])
